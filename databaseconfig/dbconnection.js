46f7319130d78a3418513a0bd05dd93e95a42016
Mario_Dou7 <mariomamdouhreda@gmail.com>
2022-12-17 14:36:22 +0200

    making queries and checks for login and register

--- databaseconfig/dbconnection.js
+++ databaseconfig/dbconnection.js
@@ -3,7 +3,7 @@ require('dotenv').config({path:'.env'})
 const MySQL = require('mysql');
 
 var Conn;
-export const connectDB = () => {
+const connectDB = () => {
     Conn = MySQL.createConnection({
         host: process.env.HOST,
         user: process.env.USER,
@@ -17,19 +17,55 @@ export const connectDB = () => {
     });
 }
 
-const find = (email,pass) => {
-    //pass = hash256(pass)
-    Conn.query(`SELECT * FROM USERS WHERE Email=${email} AND Password_SHA256=${pass}`, function (err,result) {
+
+const findall = () => {
+    Conn.query("Select * from Users", function(err, result) {
         if (err) throw err;
         console.log(result);
+    });    
+}
+
+const check_login = (name,password) => {
+    Conn.query("SELECT * FROM Users WHERE Username = ? AND Password_SHA256 = SHA2(?, 256)", [
+        name,
+        password,
+    ], function(err, data) {
+        if (err) throw err;
+        if (data.length > 0) {
+            if (name == "Admin") res.render("admin");
+            else res.render("user");
+        } else {
+            return res.render('login', {
+                message: 'Wrong username or password'
+            })
+        }
     });
 }
-find("Admin.423@email.com","pass")
-connectDB();
 
-Conn.query("Select * from Users", function(err, result) {
-    if (err) throw err;
-    console.log(result);
-});
-
-//export default connectDB;
+const register_user = (email , name , password) => {
+    Conn.query("SELECT * FROM Users WHERE Email = ?", [
+        email,
+    ], (err, result) => {
+        if (err) throw err;
+        if (result.length > 0) {
+            console.log("Email is already registered");
+            return res.render('register', {
+                message: 'This Email is already in use'
+            })
+        } else {
+            // Adding the New User to the Database
+            Conn.query("INSERT INTO Users (Username, Email, Password_SHA256) VALUES (?, ?, SHA2(?, 256))", [
+                name,
+                email,
+                password,
+            ], function(err, result) {
+                if (err) throw err;
+                return res.render('register', {
+                    success: 'Registration Successful'
+                })
+            });
+        }
+    });
+}
+    
+module.exports = { connectDB , check_login , register_user}
